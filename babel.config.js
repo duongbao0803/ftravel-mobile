e59a890ace74c0115b68785bd05147693d7e2b16
Duong <tonbao0803@gmail.com>
2024-05-16 01:10:05 +0700

    config: Set up env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,69 @@
+import {StyleSheet} from 'react-native';
+import {fontFamilies} from '../constants/fontFamilies';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+
+  text: {
+    fontFamily: fontFamilies.regular,
+    fontSize: 14,
+    color: '#120D26',
+  },
+
+  button: {
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    flexDirection: 'row',
+  },
+  shadow: {
+    shadowColor: 'rgba(0,0,0,0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 6,
+  },
+
+  section: {
+    paddingHorizontal: 16,
+    paddingBottom: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3D56F0',
+    width: 30,
+    height: 30,
+    borderRadius: 100,
+  },
+
+  tag: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#ffffff',
+    borderRadius: 100,
+    marginRight: 12,
+  },
+
+  card: {
+    padding: 12,
+    borderRadius: 12,
+    backgroundColor: '#ffffff',
+    margin: 12,
+  },
+});
