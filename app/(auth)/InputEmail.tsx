a342fd84807a59153a640e38e2008bb56ffd2475
Duong <tonbao0803@gmail.com>
2024-06-15 00:07:08 +0700

    fix: Fix receive notification

--- app/(auth)/InputEmail.tsx
+++ app/(auth)/InputEmail.tsx
@@ -8,7 +8,7 @@ import {
   Platform,
   Alert,
 } from 'react-native';
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Sms} from 'iconsax-react-native';
 import * as Animatable from 'react-native-animatable';
 import {appInfo} from '@/constants/appInfoStyles';
@@ -19,49 +19,40 @@ import {
   SpaceComponent,
 } from '@/components/custom';
 import {globalStyles} from '@/constants/globalStyles';
-import 'firebase/messaging';
-import {firebaseConfig} from '@/config/firebase';
-
-import firebase from 'firebase/app';
-import 'firebase/messaging';
-PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
-firebase.initializeApp(firebaseConfig);
+import messaging from '@react-native-firebase/messaging';
+import {Link} from 'expo-router';
 
 const InputEmail: React.FC = () => {
-  let pushToken;
-  const messaging = firebase.messaging();
-  messaging
-    .getToken()
-    .then(currentToken => {
-      if (currentToken) {
-        console.log('FCM token> ', currentToken);
-        pushToken = currentToken;
-      } else {
-        console.log('No Token available');
-      }
-    })
-    .catch((error: any) => {
-      console.log('An error ocurred while retrieving token. ', error);
-    });
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  messaging.onMessage(payload => {
-    console.log('Message received. ', payload);
-    const {title, ...options} = payload.notification;
-    navigator.serviceWorker.register('firebase-messaging-sw.js');
-    function showNotification() {
-      Notification.requestPermission(function (result) {
-        if (result === 'granted') {
-          navigator.serviceWorker.ready.then(function (registration) {
-            registration.showNotification(payload.notification.title, {
-              body: payload.notification.body,
-              tag: payload.notification.tag,
-            });
-          });
-        }
-      });
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
     }
-    showNotification();
+  }
+
+  messaging().onMessage(async remoteMessage => {
+    Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    console.log('check message', remoteMessage);
   });
+
+  const getToken = async () => {
+    try {
+      const token = await messaging().getToken();
+      console.log('FCM Token:', token);
+      return token;
+    } catch (error) {
+      console.error('Error getting FCM token:', error);
+      return null;
+    }
+  };
+
+  requestUserPermission();
+  getToken();
+
   return (
     <View style={styles.container}>
       <Animatable.View animation="fadeInRight">
